97d7dab6f07c72ed9cd3ab9130087a7022cfacda
{ADACHI} <{204081@ocsjoho.onmicrosoft.com}>
2023-05-19 12:20:58 +0900

    0519 新規登録画面とログイン画面のhtml,cssができた。 次回はjsとDBも使用して情報の取得を行っていきたい。 それが終わってからエラー処理などをしていく。

---
+++ signup.js
@@ -0,0 +1,6 @@
+document.getElementById("signup").onclick = function(){
+    const email = document.getElementById("email").value;
+    const passward = document.getElementById("pass").value;
+    const re_password = document.getElementById("re_pass").value;
+    document.write("ok");
+};
