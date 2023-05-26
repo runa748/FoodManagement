8b7fed016a067b20be77041c9338e085a58e62d3
{ADACHI} <{204081@ocsjoho.onmicrosoft.com}>
2023-05-26 12:21:00 +0900

    0526 会員情報確認ページとパスワード比較機能を作成した。 パスワード比較ではoninput=CheckPassword(this)が上手く機能せず手詰まりしてしまったが、getElementByIdで実装できた。 次回は次へボタンの機能非機能を設定していきたい。

--- signup.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("signup").onclick = function(){
-    const email = document.getElementById("email").value;
-    const passward = document.getElementById("pass").value;
-    const re_password = document.getElementById("re_pass").value;
-    document.write("ok");
-};
