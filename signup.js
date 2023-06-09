6573cc73e4878bc8e2155a471cb207a72588e0ca
{ADACHI} <{204081@ocsjoho.onmicrosoft.com}>
2023-06-09 12:27:37 +0900

    0609 MySQLインストール signup.jsの不具合は直らなかったが、クロームで動かしているとUncaught RangeError:Maximum call stack size exceededが出ていることに気づいた。だが、無限ループしている個所は見受けられず手詰まりしている。

--- signup.js
+++ signup.js
@@ -4,12 +4,16 @@ var passcount;
 var re_password;
 var asterisk;
 
-function click(){
+//document.getElementById("signup").onclick = function(){
+function onclick(){
     email = document.getElementById("email").value;
     passward = document.getElementById("pass").value;
     passcount = passward.length;
     re_password = document.getElementById("re_pass").value;
     asterisk = "";
 
-    for(i = 0;i<passcount;i++){asterisk += "*"};
+    if(passcount != undefined){
+        for(i = 0;i<passcount;i++){asterisk += "*"};
+    }
+    
 };
