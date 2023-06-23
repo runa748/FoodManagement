806af16365e0e102ce2f761de45747f1283b25e2
{ADACHI} <{204081@ocsjoho.onmicrosoft.com}>
2023-06-23 12:27:07 +0900

    06/23 以前詰まっていたところを直すにはPHPを使用しなければならないことが分かった。だが、それを使用してしまうと卒論の要件に満たないため、テーマを変更する。 このシステムは趣味で完成させる予定だ。

--- signup.js
+++ signup.js
@@ -1,14 +1,11 @@
-var email;
+/*var email;
 var password;
 var passcount;
 var re_password;
 var asterisk;
 
-
-
 //document.getElementById("signup").onclick = function(){
 function onclick(){
-    document.write("a");
     email = document.getElementById("email").value;
     password = document.getElementById("pass").value;
     passcount = password.length;
@@ -18,5 +15,23 @@ function onclick(){
     if(passcount != undefined){
         for(i = 0;i<passcount;i++){asterisk += "*"};
     }
-    
-};
+
+};*/
+function postForm(value) {
+ 
+    var form = document.createElement('form');
+    var request = document.createElement('input');
+ 
+    form.method = 'POST';
+    form.action = 'http://localhost/FoodManagement/confirmation.html';
+ 
+    request.type = 'hidden'; //入力フォームが表示されないように
+    request.name = 'text';
+    request.value = "POSTだよん";
+ 
+    form.appendChild(request);
+    document.body.appendChild(form);
+ 
+    form.submit();
+ 
+}
